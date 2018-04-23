27d27db2567b5405c5eecad855b8c903a369f2bf
root <root@stretch-amd64-casa-sencer.lab.deim>
2018-04-23 02:20:11 +0200

    "Afegits fitxers de prova i redirecciø resultat a resultat.txt"

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo -e "---Testing example file 1:---\n" 
-./a.out ExampleFiles/example.txt
-echo -e "\n---Testing example file 1:---\n"
-./a.out ExampleFiles/example2.txt
-echo -e "\n---Testing example file 1:---\n"
-./a.out ExampleFiles/example3.txt
+echo -e "---Testing example file 1:---\n" >> resultats.txt
+./a.out ExampleFiles/example.txt > resultats.txt
+echo -e "\n---Testing example file 2:---\n" >> resultats.txt
+./a.out ExampleFiles/example2.txt >> resultats.txt
+echo -e "\n---Testing example file 3:---\n" >> resultats.txt
+./a.out ExampleFiles/example3.txt >> resultats.txt
+echo -e "\n---Testing example file 4:---\n" >> resultats.txt
+./a.out ExampleFiles/example4.txt >> resultats.txt
+echo -e "\n---Testing example file 5:---\n" >> resultats.txt
+./a.out ExampleFiles/example5.txt >> resultats.txt
+echo -e "\n---Testing example file 6:---\n" >> resultats.txt
+./a.out ExampleFiles/example6.txt >> resultats.txt
+echo -e "\n---Testing example file 7:---\n" >> resultats.txt
+./a.out ExampleFiles/example7.txt >> resultats.txt
